b0fdc2e3f1bc0358eeb2e972946c0798a19588eb
jiro.kirameki <ec2-user@ip-172-31-1-68.us-east-2.compute.internal>
2018-06-18 06:24:07 +0000

    have botton

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -33,4 +33,19 @@
             'items' => $items,
         ]);
     }
+    
+     public function show($id)
+    {
+      $item = Item::find($id);
+      $want_users = $item->want_users;
+       $have_users = $item->have_users;
+
+      return view('items.show', [
+          'item' => $item,
+          'want_users' => $want_users,
+           'have_users' => $have_users,
+      ]);
+    }
+    
+    
   }
