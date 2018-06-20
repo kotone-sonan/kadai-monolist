b86d8d132502826f4f0d3f68a2d32d34ddd1baa7
jiro.kirameki <ec2-user@ip-172-31-1-68.us-east-2.compute.internal>
2018-06-20 01:04:03 +0000

    debug

--- resources/views/items/items.blade.php
+++ resources/views/items/items.blade.php
@@ -1,6 +1,6 @@
 @if ($items)
     <div class="row">
-        @foreach ($items as $item)
+        @foreach ($items as $key=> $item)
             <div class="item">
                 <div class="col-md-3 col-sm-4 col-xs-12">
                     <div class="panel panel-default">
